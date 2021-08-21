8995ecfce24ac73df9102b78197077cde8247dbc
SAIRAJ L PATGAR <69588986+SAIRAJL@users.noreply.github.com>
2021-08-21 14:26:06 +0530

    Update marks.h

--- inc/marks.h
+++ inc/marks.h
@@ -1,7 +1,14 @@
 #ifndef __MARKS_H
 #define __MARKS_H
+
 #include <stdio.h>
 #include <stdlib.h>
+#include<conio.h>
+#include<string.h>
+#include<dos.h>
+#include<stdlib.h>
+#include<alloc.h>
 #include <math.h>
+
 void marks();
 #endif
