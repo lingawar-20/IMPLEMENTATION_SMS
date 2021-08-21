4556f938f307f9d2854a2c7a2db592fd60ac0ec2
lingawar-20 <86159540+lingawar-20@users.noreply.github.com>
2021-08-21 15:30:33 +0530

    Create find.h

---
+++ inc/find.h
@@ -0,0 +1,14 @@
+#ifndef __FIND_H
+#define __FIND_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<conio.h>
+#include<string.h>
+#include<dos.h>
+#include<stdlib.h>
+#include<alloc.h>
+#include <math.h>
+
+void find();
+#endif
